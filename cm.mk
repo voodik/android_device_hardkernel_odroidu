9777ee41d51b614a80ba91d6202890dab518fe71
root <voodik.am@gmail.com>
2015-05-30 16:58:44 +0400

    updates for cm-12.1

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/hardkernel/odroidu/full_odroidu.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 #$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
